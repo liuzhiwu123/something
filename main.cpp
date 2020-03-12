31fdcebb8198eb2ae30b687b15dc705d32947037
liuwuzhi <347939046@qq.com>
2020-03-12 18:37:22 +0800

    test system

---
+++ main.cpp
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+#include<stdio.h>
+int main()
+{
+	int a=10,b=20;
+	int c=a+b;
+	return c;
+=======
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    int size=sizeof(arr/sizeof(arr[0]));
+    int index=find(arr,3);
+   cout<<index<<endl;
+    return 0;
+>>>>>>> 3f8a9991abeddaa83c869e2a727935a02545e027
+}
